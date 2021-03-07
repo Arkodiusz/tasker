b69bd5fb54247b1ee0336b61db443ffae16a22d6
Arkodiusz <arkadiusz.jedrzej@gmail.com>
2021-03-07 18:40:12 +0100

    changes according to module 23.1

--- src/main/java/com/crud/tasker/domain/card/Badges.java
+++ src/main/java/com/crud/tasker/domain/card/Badges.java
@@ -3,9 +3,11 @@ package com.crud.tasker.domain.card;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 @AllArgsConstructor
+@NoArgsConstructor
 public class Badges {
 
     @JsonProperty("votes")

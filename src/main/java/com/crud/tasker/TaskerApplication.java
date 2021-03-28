68d0318496d883f35d930c6277c209a7dbb07f8a
Arkodiusz <arkadiusz.jedrzej@gmail.com>
2021-03-28 19:19:15 +0200

    23.2 swagger io

--- src/main/java/com/crud/tasker/TaskerApplication.java
+++ src/main/java/com/crud/tasker/TaskerApplication.java
@@ -2,12 +2,19 @@ package com.crud.tasker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TaskerApplication {
+public class TaskerApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(TaskerApplication.class, args);
+    }
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(TaskerApplication.class);
     }
 
 }

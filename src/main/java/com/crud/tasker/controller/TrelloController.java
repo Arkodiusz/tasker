14d1476226c6807acec756fadd0f3ba46f23091b
Arkodiusz <arkadiusz.jedrzej@gmail.com>
2021-03-29 19:11:10 +0200

    23.4 task - email sent while creting trello card (not working)

--- src/main/java/com/crud/tasker/controller/TrelloController.java
+++ src/main/java/com/crud/tasker/controller/TrelloController.java
@@ -3,6 +3,7 @@ package com.crud.tasker.controller;
 import com.crud.tasker.domain.CreatedTrelloCard;
 import com.crud.tasker.domain.TrelloBoardDto;
 import com.crud.tasker.domain.TrelloCardDto;
+import com.crud.tasker.service.TrelloService;
 import com.crud.tasker.trello.client.TrelloClient;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
@@ -16,14 +17,15 @@ import java.util.List;
 public class TrelloController {
 
     private final TrelloClient trelloClient;
+    private final TrelloService trelloService;
 
     @GetMapping("getTrelloBoards")
     public List<TrelloBoardDto> getTrelloBoards() {
-        return trelloClient.getTrelloBoards();
+        return trelloService.fetchTrelloBoards();
     }
 
     @PostMapping("createTrelloCard")
     public CreatedTrelloCard createTrelloCard(@RequestBody TrelloCardDto trelloCardDto) {
-        return trelloClient.createNewCard(trelloCardDto);
+        return trelloService.createTrelloCard(trelloCardDto);
     }
 }

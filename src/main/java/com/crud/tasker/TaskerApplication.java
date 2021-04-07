ad4eaf181899ed9281a93067651a0a7cf1eac125
Arkodiusz <arecki358@gmail.com>
2021-04-07 19:39:03 +0200

    31 module heroku config

--- src/main/java/com/crud/tasker/TaskerApplication.java
+++ src/main/java/com/crud/tasker/TaskerApplication.java
@@ -6,15 +6,15 @@ import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class TaskerApplication extends SpringBootServletInitializer {
+public class TaskerApplication { //} extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(TaskerApplication.class, args);
     }
 
-    @Override
-    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
-        return application.sources(TaskerApplication.class);
-    }
+//    @Override
+//    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+//        return application.sources(TaskerApplication.class);
+//    }
 
 }

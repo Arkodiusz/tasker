a00e705e42d08186081b00d29f8bb9c04c346f4a
Arkodiusz <arecki358@gmail.com>
2021-04-18 18:04:46 +0200

    32_3 module - conditions in html template IF

--- src/main/java/com/crud/tasker/service/MailCreatorService.java
+++ src/main/java/com/crud/tasker/service/MailCreatorService.java
@@ -21,11 +21,13 @@ public class MailCreatorService {
         Context context = new Context();
         context.setVariable("MESSAGE_CONTENT", message);
         context.setVariable("TASKER_URL", "http://Arkodiusz.github.io");
-        context.setVariable("ADMIN_NAME", adminConfig.getAdminName());
+        context.setVariable("adminConfig", adminConfig);
         context.setVariable("BUTTON_NAME", "Visit website");
         context.setVariable("PREVIEW_MESSAGE", "NOTIFICATION");
         context.setVariable("GOODBYE_MESSAGE", "Bye bye!");
-        context.setVariable("COMPANY_DETAILS", adminConfig.getCompanyName());
+
+        context.setVariable("show_button", false);
+        context.setVariable("is_friend", true);
 
         return templateEngine.process("mail/created-trello-card-mail", context);
     }

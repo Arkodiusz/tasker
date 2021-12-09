7cff20d90a9a82f7a9a5ecfdb7d9098d6c09356c
Arkodiusz <arecki358@gmail.com>
2021-12-09 23:58:52 +0100

    cors

--- src/main/java/com/crud/tasker/controller/StaticWebPageController.java
+++ src/main/java/com/crud/tasker/controller/StaticWebPageController.java
@@ -3,12 +3,14 @@ package com.crud.tasker.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Map;
 
 @Controller
+@CrossOrigin(origins = "*")
 public class StaticWebPageController {
 
     @RequestMapping("/")
